64e1e24737a4d928f9c98bf912754c8e6363c649
Bonaventure Ani <okwudiribonaventure@gmail.com>
2025-04-18 15:30:29 +0100

    Factory function and unit test tdd

--- data-structures/__tests__/stacks.test.js
+++ data-structures/__tests__/stacks.test.js
@@ -1,1 +1,23 @@
 // create test suite for stack data structure here
+const { createStack } = require('../stack');
+
+describe('Stack', () => {
+  test('should create an empty stack', () => {
+    const stack = createStack();
+    expect(stack.quantity).toEqual(0);
+  });
+
+  test('should initialize with an empty storage object', () => {
+    const stack = createStack();
+    expect(stack.storage).toEqual({});
+  });
+  test('should have a max size of 5', () => {
+    const stack = createStack();
+    expect(stack.maxSize).toEqual(5);
+  });
+  test('maxsize should take an arugment of 10', () => {
+    const stack = createStack(10);
+    expect(stack.maxSize).toEqual(10);
+  });
+
+});
--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,5 +1,10 @@
-function createStack() {
+function createStack(maxSize = 5) {
   // build your stack object inside this factory function
+  let stack = {};
+  stack.storage = {};
+  stack.quantity = 0;
+  stack.maxSize = maxSize;
+  return stack;
 }
 
-module.exports = createStack;
+module.exports = { createStack };
